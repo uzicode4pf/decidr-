c9b1bfae3ec4a6f1702bef60a3f489395d399e17
Algo Bender <abisoyelekuti@gmail.com>
2025-09-29 20:39:44 +0100

    Add tempates folder and update man.py for frontend routes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from app.routers import polls
 
 app = FastAPI()
 
-@app.get("/")
+app.mount("/static",
+          StaticFiles(directory="static"),
+          name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/servertest")
 def read_root():
     return {"message": "hello, Decidr is ready!"}
 
