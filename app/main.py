ef9bf4b10b4b37af140988fd923979bd7faef39c
Raji Mahmud <raji.mahmud.a@gmail.com>
2025-09-30 10:05:05 +0100

    served static files

--- app/main.py
+++ app/main.py
@@ -16,6 +16,19 @@ templates = Jinja2Templates(directory="templates")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.get("/create", response_class=HTMLResponse)
+async def create(request: Request):
+    return templates.TemplateResponse("create.html", {"request": request})
+
+@app.get("/results", response_class=HTMLResponse)
+async def results(request: Request):
+    return templates.TemplateResponse("results.html", {"request": request})
+
+@app.get("/vote", response_class=HTMLResponse)
+async def vote(request: Request):
+    return templates.TemplateResponse("vote.html", {"request": request})
+
+
 @app.get("/servertest")
 def read_root():
     return {"message": "hello, Decidr is ready!"}
